83ee6391f6603a12ad50e1caee68bc3870720142
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-05-04 20:18:00 -0700

    Update for Review 2

--- Program.cs
+++ Program.cs
@@ -7,6 +7,222 @@
 
         static void Main(string[] args)
         {
+            //double rateCheck = 72 / 5.0;
+            //Console.WriteLine(rateCheck);
+
+            Menu();
+        } // ----------------
+
+        // Midterm Practice
+
+        // Making a small menu that the user can choose an option from
+
+        public static void Menu()
+        {
+            // Ask the user for their location information, return the result
+            //string userLocation = ReturnUserLocation();
+            string userLocation = FormatLocation("Starbucks", "Seattle"); // Remember to remove
+
+            // Greeting
+            Console.WriteLine(StringGreeting(userLocation));
+            Console.WriteLine("--------------------------------------");
+
+            string choice1 = "Peach Green Tea";
+            double price1 = 3.75;
+
+
+            string choice2 = "Black Ice Tea";
+            double price2 = 3.25;
+
+            // I want to display my menu ATLEAST once
+            // So i will wrap my code in a do while
+            // because a DO While loop is guanenteed to run atleast once
+            string userContinue;
+            double total = 0;
+            do
+            {
+                // Start my loop here
+                Console.WriteLine($"Your local store is {userLocation}");
+                Console.WriteLine("What would you like to buy today?");
+                Console.WriteLine($"1 - {choice1}");
+                Console.WriteLine($"2 - {choice2}");
+                Console.Write("Which would you like?");
+                string userChoice = Console.ReadLine();
+
+                // Condition
+                // Let the user CHOOSE which drink to buy
+                
+
+                if (userChoice == "1") // If they choose the green tea
+                {
+                    Console.WriteLine($"You have chosen {choice1}");
+                    total += DrinkPrice(price1);
+                }
+                else if (userChoice == "2") // if they choose the black tea
+                {
+                    Console.WriteLine($"You have chosen {choice2}");
+                    total += DrinkPrice(price2);
+
+                }
+                else // if they put the wrong drink
+                {
+                    Console.WriteLine("You have entered an invalid choice");
+                }
+
+                Console.WriteLine($"Your drink costs {total.ToString("c")}");
+
+                DisplayTotal(total);
+
+
+                Console.WriteLine("Would you like anything else today? Type n to exit");
+                userContinue = Console.ReadLine();
+                // End my loop here
+            } while (userContinue != "n");
+
+            
+
+
+            // I was testing to make sure that it recognizes when the user says NO, i dont' want another drink
+            //if (userContinue != "n")
+            //{
+            //    Console.WriteLine("Ask for another drink");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Exit the app");
+            //}
+            
+
+        } // 
+
+        public static void DisplayTotal(double amount)
+        {
+            // Display after tax
+
+            double tax = .11;
+            double drinkTax = amount * tax;
+            double totalAmount = amount + drinkTax;
+
+            Console.WriteLine($"Your drink comes to a total of  {totalAmount.ToString("c")}: The tax was {drinkTax.ToString("c")}");
+
+        }
+
+        public static double DrinkPrice(double amount)
+        {
+            // I want to write code for the size
+            Console.WriteLine("What size would you like?");
+            Console.WriteLine("1 - Tall");
+            Console.WriteLine("2 - Grande");
+            Console.WriteLine("3 - Vente");
+            string userSizeChoice = Console.ReadLine();
+
+            // Now I'm keeping track of a price
+            double total = 0;
+
+            if (userSizeChoice == "1")
+            {
+                total += amount;
+            }
+            else if (userSizeChoice == "2")
+            {
+                total += amount + (amount * .25);
+            }
+            else if (userSizeChoice == "3")
+            {
+                total += amount + (amount * .5);
+            }
+
+            return total;
+        } // DrinkPrice
+
+        public static string ReturnUserLocation()
+        {
+            // Displaying a greeting
+            Console.Write("Where are you located: "); // Output - Display the question to the user
+            string location = Console.ReadLine(); // Input - Getting the user information
+            Console.Write("What store are you visiting: "); // Output - Display question
+            string store = Console.ReadLine(); // Input - Get the users store location
+
+            // Variable and operation : Formatting the string and saving the value
+            return FormatLocation(store, location);
+        }
+
+        // It will loop as long as the user wants it to
+        // when they make a choice it will keep track of the choices made
+        // total the cost of the users choices
+        // it will display a total
+
+        // 6 fundamentals
+        // inputs
+        // outputs
+        // loops
+        // conditions
+        // variables
+        // operations
+
+        // -------------------------------
+
+        public static void MethodExample()
+        {
+            // We call a method by its NAME and passing in arguments
+            //GreetingExample("Sams Club");
+            //GreetingExample("Costco");
+            //GreetingExample("Costco", "Tukwila");
+            string location = "Issaquah";
+            string store = "Costco";
+            string formattedLocation = FormatLocation(store, location); // This would be "Issaquah Costco"
+
+            Console.WriteLine(StringGreeting(formattedLocation));
+
+
+            //Console.WriteLine(FormatLocation("Fred Meyer", "Benson Plaza"));
+        }
+
+        // Methods - Code blocks, that do one specific task / process / action / function
+        // They are a block of code, given a name, that runs the block of code when called
+        // public static Return Type - Name - Parameters - Code Block
+        // Return type - tells the computer what TYPE of data is coming out of your method
+        // VOID : If nothing is being returned
+        // Common Error
+        // There is no information being passed into it, and nothing coming out of it
+        public static void VoidReturnExample()
+        {
+            // Nothing is being returned, it just performs an Action
+            Console.WriteLine("Welcome to Auburn Sams Club");
+        }
+
+        // In the parentheses I'm going to pass in a string value
+        public static void GreetingExample(string companyName, string location)
+        {
+            // String literal
+            string formatConCatenate = "Welcome to the " + location + " " + companyName;
+
+            // \n to go to next line inside double quotes
+            // The \n is an escape character
+            // These are special characters inside our strings, that format information
+            // \n - For new line
+            // \t - For Tab
+            // \\ - To show the \
+            string formatStringLiteral = $"Welcome to the {location} {companyName}";
+            Console.WriteLine(formatStringLiteral);
+        }
+
+        public static string StringGreeting(string storeAndLocation)
+        {
+            string formatted = $"Greetings! And welcome to the {storeAndLocation}";
+            // use the return key word
+            return formatted;
+
+        }
+
+        public static string FormatLocation(string companyName, string location)
+        {
+            return $"{location} {companyName}";
+        }
+
+
+        static void Loop()
+        {
             //string userInput = Console.ReadLine();
 
             //while(userInput != "Will")
@@ -44,7 +260,6 @@
 
 
             //Console.WriteLine("After our loop");
-
         }
 
         static void ConditionExample()
@@ -91,10 +306,7 @@
             // -52,382.55
         }
 
-        static void MethodExample()
-        {
-            Console.WriteLine(name);
-        }
+      
 
         static void Example()
         {
